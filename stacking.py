07d66a4cbb700dd999dcad362f599bfa1157b631
Patrick Horlaville <horlaville@mussel.cita.utoronto.ca>
2023-07-18 23:36:32 -0400

    updating paper figures

--- stacking.py
+++ stacking.py
@@ -1,5 +1,5 @@
 from stacking_params import *
-
+plt.rcParams["mathtext.fontset"] = "dejavuserif"
 
 
 # Calculating luminosities
@@ -16,9 +16,9 @@ halo_ys = lim_sim.halos.dec[thresh][good_halo_zs]
 halo_zs = halo_zs[good_halo_zs]
 
 print('------------------------')
-print(' - The total forecast observing time has been set to', t_obs, '.')
-print(' - Redshift of selected slice is', round(map_zs[ind], 2), 'and accepted halos are in the redshift range [', round(map_zs[ind] - err, 2), ',', round(map_zs[ind] + err, 2), '].')
-print(' - Stacked map is', n, 'by', n, 'which covers', stack_dim, 'deg by', stack_dim, 'deg.')
+print(' - The total forecast observing time has been set to', t_obs, '-')
+print(' - Redshift of selected slice is', round(map_zs[ind], 2), 'and accepted halos are in the redshift range [', round(map_zs[ind] - err, 2), ',', round(map_zs[ind] + err, 2), '] -')
+print(' - Stacked map is', n, 'by', n, 'which covers', stack_dim, 'deg by', stack_dim, 'deg -')
 print('------------------------')
 
 
@@ -29,33 +29,25 @@ pure_stack, noisy_stack = np.nanmean(pure_map, axis = 0), np.nanmean(noisy_map, 
 
 # Plotting
 
-fig , axes = plt.subplots(nrows = 2, ncols = 1, figsize = (8, 16))
+fig , axes = plt.subplots(nrows = 2, ncols = 1, figsize = (10, 16))
 
 plt.subplot(211)
 plt.imshow(pure_stack, cmap = 'CMRmap', extent = [-stack_dim/2, stack_dim/2, -stack_dim/2, stack_dim/2])
-plt.title(r'$Pure\ Signal\ Stacked\ Map$', math_fontfamily = 'dejavuserif')
-plt.xlabel(r'$RA\ [Degrees]$', math_fontfamily='dejavuserif')
-plt.ylabel(r'$Dec\ [Degrees]$', math_fontfamily='dejavuserif')
-cb1 = plt.colorbar(label = r'$Jy/sr$')
-ax1 = cb1.ax
-text1 = ax1.yaxis.label
-font = matplotlib.font_manager.FontProperties(math_fontfamily='dejavuserif', style='italic')
-text1.set_font_properties(font)
+plt.title(r'$Pure\ Signal\ Stacked\ Map$')
+plt.ylabel(r'$Dec\ (Degrees)$')
+plt.colorbar(label = r'$Flux\ (Jy/sr)$')
 
 
 plt.subplot(212)
 plt.imshow(noisy_stack, cmap = 'CMRmap', extent = [-stack_dim/2, stack_dim/2, -stack_dim/2, stack_dim/2])
-plt.title(r'$Forecast\ Stacked\ Map$', math_fontfamily = 'dejavuserif')
-plt.xlabel(r'$RA\ [Degrees]$', math_fontfamily='dejavuserif')
-plt.ylabel(r'$Dec\ [Degrees]$', math_fontfamily='dejavuserif')
-cb2 = plt.colorbar(label = r'$Jy/sr$')
-ax2 = cb2.ax
-text2 = ax2.yaxis.label
-font = matplotlib.font_manager.FontProperties(math_fontfamily='dejavuserif', style='italic')
-text2.set_font_properties(font)
+plt.title(r'$Forecast\ Stacked\ Map$')
+plt.xlabel(r'$RA\ (Degrees)$')
+plt.ylabel(r'$Dec\ (Degrees)$')
+plt.colorbar(label = r'$Flux\ (Jy/sr)$')
 
-#fig.tight_layout()
-plt.savefig('Stacking/stacking_ngauss_june12.png', bbox_inches = "tight")
+
+plt.savefig('Stacking/jul18_270GHz_n50_t.png', bbox_inches = "tight")
+
 plt.show()
 
 
--- stacking_params.py
+++ stacking_params.py
@@ -8,20 +8,23 @@ lim_sim = lim('Lichen_v4', doSim=True)
 # Be aware: make sure to adapt nuObs and Delta_nu to your lightcone
 # dnu for the amount of redshift slices
 
-t_obs = 2000 * u.hr
+t_obs = 40000 * u.hr
 
+# [C II] example:
 lim_sim.update(model_par = {'zdex': 0.3,
- 'M0': 1900000000.0,
- 'Mmin': 20000000000,
- 'alpha_MH1': 0.74,
- 'alpha_LCII': 0.017,
- 'BehrooziFile': 'sfr_reinterp.dat'}, 
-             tobs = t_obs,
-             nuObs = 400*u.GHz,
-             Delta_nu = 28*u.GHz,
-             Omega_field = 16*(u.deg**2),
-               catalogue_file = '/mnt/scratch-lustre/horlaville/nate_sims/june_9/ngaussian/ngauss_mlambda25_0.npz'
+                            'M0': 1900000000.0,
+                            'Mmin': 20000000000,
+                            'alpha_MH1': 0.74,
+                            'alpha_LCII': 0.017,
+                            'BehrooziFile': 'sfr_reinterp.dat'},
+               tobs = t_obs,
+               nuObs = 270*u.GHz,
+               Delta_nu = 40*u.GHz,
+               Omega_field = 4*(u.deg**2),
+               dnu = 1.11*u.GHz,
+               catalogue_file = '/home/dongwooc/scratchspace/pprun_hiz_npz/COMAP_z5.8-7.9_960Mpc_seed_13819.npz'
             )
+
 
 # Lower bound of halo masses to be considered
 mass_cut = 2*(10**10) # in Solar masses
@@ -29,11 +32,11 @@ mass_cut = 2*(10**10) # in Solar masses
 # Error of acceptance on the redshift of the 2D map for surrounding halos
 err = 0.03
 
-# Index of the redshift map to be selected. There are 21 redshift slices by default, so ind can be any integer between 0 and 20
-ind = 3
+# Index of the redshift map to be selected.
+ind = 17
 
 # Size of the stacked map to be produced (n by n)
-n = 75
+n = 50
 
 # Size of the stacked map, in angular dimension
 ang_side = np.sqrt(lim_sim.Omega_field)
